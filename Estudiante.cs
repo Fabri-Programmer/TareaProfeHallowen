448ddee25eb3cedf7521d332d7a4178659a59fe2
Fabri-Programmer <fabrizziopks@hotmail.com>
2024-10-31 12:16:59 -0500

    TareaHallowin

---
+++ Estudiante.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaDeHoyHallowen
+{
+    internal class Estudiante
+    {
+        int[] edades = new int[0];
+        int posicion = 0;
+        string seguir;
+
+        public void insertar()
+        {
+            do
+            {
+                
+
+                int ed;
+                Console.WriteLine("Ingresa una edad: ");
+
+                while (!int.TryParse(Console.ReadLine(), out ed) || ed < 14 || ed > 120)
+                {
+                    Console.WriteLine("Error: Ingrese otra vez: ");
+                }
+                Array.Resize(ref edades, edades.Length + 1);
+                edades[posicion] = ed;
+                posicion++;
+                Console.WriteLine("Edad ingresada correctamente");
+                Console.Write("\nDesea seguir registrando: ");
+            } while (seguir == "si");
+        }
+        public void mostrar()
+        {
+            Console.SetCursorPosition(10, 12);
+            Console.Write("Position");
+            Console.SetCursorPosition(30, 12);
+            Console.Write("EDADES");
+            for(int i= 0; i< edades.Length; i++)
+            {
+                Console.SetCursorPosition(10, 13+i);
+                Console.Write(i);
+                Console.SetCursorPosition(30, 13+i);
+                Console.Write(edades[i]);
+
+
+            }
+
+        }
+        public void eliminar(int ed1)
+        {
+            int indice = Array.IndexOf(edades, ed1);
+            if(indice != -1)
+            {
+                for (int i = indice; i < edades.Length-1; i++)
+                {
+                    edades[i] = edades[i + 1];
+                }
+                Array.Resize(ref edades, edades.Length-1);
+                posicion--;
+                Console.Write("\nEliminado correctamente");
+            }
+            else
+            {
+                Console.WriteLine("\nNo se puede eliminar Porque no existe");
+            }
+        
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿
+using TareaDeHoyHallowen;
+
+string ha;
+int n;
+Estudiante e = new Estudiante();
+do
+{
+
+
+Console.WriteLine("Bienvenido al sistema de registro de edades\n");
+Console.WriteLine("****** MENU *******");
+Console.WriteLine("*1 Insertar       *");
+Console.WriteLine("*2 Mostrar        *");
+Console.WriteLine("*3 Eliminar       *");
+Console.WriteLine("*4 Mostrar        *");
+Console.WriteLine("*0 Salir          *");
+Console.WriteLine("*******************\n");
+
+Console.Write("ingrese una opcion: ");
+    
+
+
+while(!int.TryParse(Console.ReadLine(), out n)|| n>4)
+{
+    Console.Write("Error: Ingrese opcion: ");
+}
+
+switch (n)
+{
+    case 0: Environment.Exit(0); break;  
+    case 1:  e.insertar(); break;
+    case 2: e.mostrar();  break;
+    case 3:
+            Console.Write("\n Ingrese la edad a eliminar: ");
+            int ed = int.Parse(Console.ReadLine());
+            e.eliminar(ed); break;
+    case 4: break;
+
+}
+Console.Write("\nDesea regresar al menu: ");
+ ha = Console.ReadLine();
+    Console.Clear();
+    
+}while (ha == "si") ;
+
+
+
+
